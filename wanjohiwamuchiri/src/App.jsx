6f68a3be6a50d344edc726eab3f1110506ffcb4f
wanjohiwamuchiri <wanjohimuchiri@protonmail.com>
2023-05-10 14:34:49 +0300

    fixes

--- wanjohiwamuchiri/src/App.jsx
+++ wanjohiwamuchiri/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" >
+        <a href="https://vitejs.dev">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev">
